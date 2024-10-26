5f3ce5f455d8e9193791e8c68f610003df966886
Yongo David <davidyongo9@gmail.com>
2024-10-26 21:07:35 +0100

    refresh

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
+import { Provider } from "@/components/ui/provider"
+// import { ChakraProvider } from '@chakra-ui/react'
 import HomePage from './HomePage';
 function App() {
   return (
-    <HomePage /> 
+    <Provider>
+      <HomePage /> 
+    </Provider>
   );
 }
 export default App;
